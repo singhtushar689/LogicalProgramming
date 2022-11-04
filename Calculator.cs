6b04676b65ea48c7cff3a97df43c6a29137803a0
singhtushar689 <singhtushar689@gmail.com>
2022-11-04 23:10:40 +0530

    product of two no.

---
+++ Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatIons
+{
+    public class Calculator
+    {
+        public static void Multiplication()
+        {
+            Console.WriteLine("Enter the First Number");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number");
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Product of the Number is:" + A * B);
+        }
+
+    }
+}
